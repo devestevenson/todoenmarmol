9bef9957079a1522bc5c8ebc2b4f741e7d0612af
devestevenson <devestevenson@gmail.com>
2024-05-31 19:20:30 -0500

    add logo footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,15 +1,16 @@
-import logoFoot from "../../assets/icons/logofoot.png";
+import logoFoot from "../../assets/icons/logotodoenmarmol.svg";
 import iconWapp from "../../assets/icons/whatsapp.svg";
 import iconEmail from "../../assets/icons/envelope.svg";
 import iconIg from "../../assets/icons/instagram.svg";
 import iconFb from "../../assets/icons/facebook.svg";
+import { Img } from "react-image";
 
 const Footer = () => {
 
   return (
     <section className="flex flex-col justify-center items-center py-16 bg-tmwhite max-w-[1280px] responsiveWeb mx-auto">
       <picture className="block max-w-52 h-24 overflow-hidden">
-        <img
+        <Img
           className="w-full h-full object-cover"
           src={logoFoot}
           alt=""
@@ -20,22 +21,22 @@ const Footer = () => {
 
       <ul className="w-full flex flex-col gap-4 justify-between items-center  lg:flex-row">
         <li className="flex items-center gap-1">
-          <img src={iconWapp} alt="whatsapp" />
+          <Img src={iconWapp} alt="whatsapp" />
           <a href="https://wa.link/o195ma" target="_blank" className="text-lg">(+57)324 583 8015</a>
         </li>
 
         <li className="flex items-center gap-1">
-          <img src={iconEmail} alt="correo electrónico" />
+          <Img src={iconEmail} alt="correo electrónico" />
           <a href="mailto:todoenmarmol08@gmail.com" target="_blank" className="text-lg">todoenmarmol08@gmail.com</a>
         </li>
 
         <li className="flex items-center gap-1">
-          <img src={iconIg} alt="instagram" />
+          <Img src={iconIg} alt="instagram" />
           <a href="https://www.instagram.com/todoenmarmol.col" target="_blank" className="text-lg">@todoenmarmol.col</a>
         </li>
 
         <li className="flex items-center gap-1">
-          <img src={iconFb} alt="facebook" />
+          <Img src={iconFb} alt="facebook" />
           <a href="https://www.facebook.com/todoenmarmoleria" target="_blank" className="text-lg">Todo en Mármol</a>
         </li>
       </ul>
